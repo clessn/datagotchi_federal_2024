5618e970903047ea05db044553019669ccf4aee7
AlexBouillon <alexandrebouillon7@gmail.com>
2024-11-26 09:52:59 -0500

    début script pred

--- clustering/cluster_evolution/generate_predictions_by_day.R
+++ clustering/cluster_evolution/generate_predictions_by_day.R
@@ -1,6 +1,18 @@
-Loader le modèle du 1er script
-Loader le kmeans_result
-Loader les données de lapp
+
+# Packages ---------------------------------------------------------------
+
+
+
+
+# Data -------------------------------------------------------------------
+
+model <- readRDS("multinom_model.rds")
+
+kmeans_result <- readRDS("multinom_model.rds")
+
+app_data <- read.csv("data")
+
+
 Loop: pour chaque jour, faire un modèle bayésien vote_intent ~ SES + lifestyle qui prend comme priors les coefficients du modèle du 1er script
 Prédire ce modèle pour chaque centroid
 Enregistrer une dataframe avec la prédiction de chaque cluster par jour
