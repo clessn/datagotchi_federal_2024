37e07b383f833974594a7f485a43914a6de6801a
AlexBouillon <alexandrebouillon7@gmail.com>
2025-01-05 22:04:25 -0500

    model 2022

--- clustering/exploration/qc_2022/4-2022_model.R
+++ clustering/exploration/qc_2022/4-2022_model.R
@@ -1,7 +1,11 @@
+# Load packages ----------------------------------------------------------
+library(nnet)
+library(dplyr)
+library(tidyr)
 
+# Data -------------------------------------------------------------------
 
-
-
+df_pilot1_2022 <- read.csv("/home/alexab/Dropbox/Ulaval/CLESSN/_SharedFolder_datagotchi-developpement/quebec_prov_2022/pilotes/pilote-1-quebec-prov-2022.csv")
 
 variables_int <- c(
   # "id",
@@ -181,3 +185,130 @@ variables_int <- c(
    "ses_bisex"#,
   # "ses_sexOri_other"#,
  )
+
+# Sélection des variables inclues dans le clustering et celles de voteIntent
+data_prior <- df_pilot1_2022 %>%
+  select(all_of(variables_int)) %>%
+  drop_na()
+
+# Création de la variable cible (vote_intent) 
+data_prior <- data_prior %>%
+  mutate(
+    vote_intent = case_when(
+      op_intent_CAQ == 1 ~ "CAQ",
+      op_intent_PQ == 1 ~ "PQ",
+      op_intent_PLQ == 1 ~ "PLQ",
+      op_intent_QS == 1 ~ "QS",
+      op_intent_PCQ == 1 ~ "PCQ",
+      op_intent_Other == 1 ~ "Autre",
+      op_intent_wontVote == 1 ~ "NoVote",
+      TRUE ~ NA_character_
+    )
+  ) %>%
+  select(-starts_with("op_voteIntent_")) %>% 
+  drop_na(vote_intent) 
+
+#diminution marquée du nombre de répondants car:
+# > table(df_pilot1_2022$op_intent_dontKnow)
+#    0    1 
+# 1196  304 
+
+
+# Conversion en facteur
+data_prior$vote_intent <- as.factor(data_prior$vote_intent)
+levels(data_prior$vote_intent)
+data_prior$vote_intent <- relevel(data_prior$vote_intent, ref = "CAQ")
+
+data_model <- data_prior %>%
+  select(
+    vote_intent,
+    male,
+    female,
+    ses_genderOther,
+    age34m,
+    age3554,
+    age55p,
+    langEn,
+    langFr,
+    ses_languageOther,
+    act_Gym,
+    act_TeamSport,
+    act_Run,
+    act_Yoga,
+    act_None,
+    act_Fishing,
+    act_Hunting,
+    act_VisitsMuseumsGaleries,
+    act_MotorizedOutdoorActivities,
+    act_Volunteering,
+    animal_cat,
+    animal_dog,
+    cons_brand_MaR,
+    cons_brand_Frip,
+    educBHS,
+    educCollege,
+    educUniv,
+    cons_redWineDrink,
+    cons_regBeers,
+    cons_cocktailsDrink,
+    cons_noDrink,
+    incomeLow,
+    incomeMid,
+    incomeHigh,
+    ses_dwelling_app,
+    ses_dwelling_detachedHouse,
+    act_transport_Car,
+    act_transport_Walk,
+    act_transport_PublicTransportation,
+    vehicule_PickUp,
+    vehicule_noCar,
+    immigrant,
+    cons_coffee_TimH,
+    cons_coffee_Starbucks,
+    cons_coffee_place_noCoffee,
+    app_noTattoo,
+    cons_low_Meat,
+    cons_mid_Meat,
+    cons_much_Meat,
+    ses_ethn_White,
+    ses_ethn_Black,
+    ses_ethn_Asiatique,
+    ses_hetero,
+    ses_gai,
+    ses_bisex
+  )
+
+# Modèle -----------------------------------------------------------------
+multinom_model <- multinom(vote_intent ~ ., data = data_model)
+
+summary <- summary(multinom_model)
+
+# Interprétation des coefficients -----------------------------------------
+# Les coefficients peuvent être transformés en probabilités relatives
+exp(coef(multinom_model))
+
+summary$coefficients
+summary$standard.errors
+
+# Prédictions ------------------------------------------------------------
+# Prédiction sur les données utilisées pour l'entraînement
+data_model$predictions <- predict(multinom_model, newdata = data_model)
+
+# Évaluation de la performance --------------------------------------------
+# Matrice de confusion
+table(data_model$vote_intent, data_model$predictions)
+
+# Sauvegarder en RDS -----------------------------------------------------
+
+saveRDS(multinom_model, file = "_SharedFolder_datagotchi_federal_2024/clustering/data/multinom_model_2022.rds")
+
+# Calcul de l'exactitude globale
+correct_predictions <- sum(diag(table(data_model$vote_intent, data_model$predictions)))
+total_predictions <- sum(table(data_model$vote_intent, data_model$predictions))
+accuracy <- correct_predictions / total_predictions
+print(paste("Exactitude Globale :", round(accuracy * 100, 2), "%"))
+
+# Matrice de confusion normalisée par ligne (classes réelles)
+confusion_matrix <- table(data_model$vote_intent, data_model$predictions)
+confusion_matrix_normalized <- prop.table(confusion_matrix, 1)
+print(round(confusion_matrix_normalized, 2))
