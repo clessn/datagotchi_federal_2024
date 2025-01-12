14780c27333a03e15ddb805c0e3fe3c497d692f6
etienneprx <etienne.proulx.2@ulaval.ca>
2025-01-12 13:25:17 -0500

    donnees pilote

--- code/cleaning/master_cleaning_fed_2024.R
+++ code/cleaning/master_cleaning_fed_2024.R
@@ -5,7 +5,7 @@ library(dplyr)
 
 ## load raw data here
 
-data_raw <- haven::read_sav("_SharedFolder_datagotchi_federal_2024/data/data_raw/datagotchi_federal_pilot_November+26,+2024_12.40.sav")
+data_raw <- haven::read_sav("_SharedFolder_datagotchi_federal_2024/data/data_raw/datagotchi_federal_pilot_January+10,+2025_15.22.sav")
 
 # Clean variables ---------------------------------------------------------
 
