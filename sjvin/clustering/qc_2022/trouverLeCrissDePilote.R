46255ce6cff51b9ed324e1e34a797465c0c317a2
sarahjanevincent <141277350+sarahjanevincent@users.noreply.github.com>
2025-01-31 13:14:44 -0500

    test pour trouver les maudites variables dans le bordel de 2022

---
+++ sjvin/clustering/qc_2022/trouverLeCrissDePilote.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+
+# Load data --------------------------------------------------------------
+
+CleanData_Pilote1 <- read.csv("../_SharedFolder_datagotchi-2022/Data/Clean/_pilotes2022/CleanData-Pilote1.csv")
+Pilote1 <- read.csv("../_SharedFolder_datagotchi-2022/Data/Clean/_pilotes2022/Pilote1.csv")
+RawData <- read.csv("../_SharedFolder_datagotchi-2022/Data/Raw/RawData-Hub.csv")
