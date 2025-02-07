d44f79ba6f100f432004fab2269fa957c2e200e3
AlexBouillon <alexandrebouillon7@gmail.com>
2025-02-07 10:32:31 -0500

    06 fini, v0 graph

--- albou558/qc_2022/06_voteIntent_par_jour.R
+++ albou558/qc_2022/06_voteIntent_par_jour.R
@@ -16,15 +16,20 @@ appData <- readRDS("_SharedFolder_datagotchi_federal_2024/data/clustering/qc_202
 dfVote <- appData |> 
   select(id, op_intent, time)
 
-data <- app_data_cluster |> 
-left_join(dfVote, by = "id") |> 
-  drop_na()
+data <- app_data_cluster %>% 
+  left_join(dfVote, by = "id") %>% 
+  drop_na() %>%
+  # Convertir la variable time en Date si nécessaire et filtrer
+  mutate(time = as.Date(time)) %>% 
+  filter(time <= as.Date("2022-10-03"))
 
 # Fonction pour analyser le voteIntent par jour
 fct_06_analyze_voteintent_by_day <- function(data) {
-  # 1. Ajouter une colonne "day" si elle n'existe pas déjà
+  # 1. S'assurer que time est bien de type Date
+  data <- data %>% mutate(time = as.Date(time))
+  
+  # Création de la variable "day" si nécessaire
   if (!"day" %in% names(data)) {
-    data$time <- as.Date(data$time)
     unique_dates <- sort(unique(data$time))
     date_to_day_number <- data.frame(
       time = unique_dates,
@@ -35,17 +40,30 @@ fct_06_analyze_voteintent_by_day <- function(data) {
       arrange(time)
   }
   
-  # 2. Agréger les résultats par jour et par cluster
+  # 2. Agréger : compter le nombre de réponses par jour, par cluster et par parti
   results_by_day <- data %>%
+    group_by(time, cluster, op_intent) %>%
+    summarise(n = n(), .groups = "drop") %>%
     group_by(time, cluster) %>%
-    summarise(
-      total_respondents = n(),
-      mean_vote_intent = mean(op_intent, na.rm = TRUE),
-      .groups = "drop"
-    )
+    mutate(total = sum(n),
+           proportion = n / total) %>%
+    ungroup()
   
   return(results_by_day)
 }
 
+
 # Appliquer la fonction pour obtenir les résultats par jour
 voteintent_by_day <- fct_06_analyze_voteintent_by_day(data)
+
+
+library(ggplot2)
+
+results_by_day <- fct_analyze_voteintent_by_day(data)
+
+ggplot(results_by_day, aes(x = time, y = proportion, color = op_intent)) +
+  geom_line() +
+  facet_wrap(~ cluster) +
+  labs(title = "Évolution du vote intent par cluster",
+       x = "Date", y = "Proportion de répondants") +
+  theme_minimal()
