ec584a2396a2363685ddc2e7a487e1abbfaa2a99
AlexBouillon <alexandrebouillon7@gmail.com>
2025-02-13 10:22:21 -0500

    début carte

---
+++ albou558/qc_2022/03_02-3_desc_geographique.R
@@ -0,0 +1,12 @@
+library(clessnize)
+library(tidyverse)
+library(sf)
+
+
+shp <- sf::read_sf("_SharedFolder_datagotchi_federal_2024/data/shapefile/qc2022/Circonscription_electorale_2022_shapefile.shp") 
+
+ggplot(shp) +
+  geom_sf(color = "grey20") +
+  theme_void()
+
+sf::geom_sf()
