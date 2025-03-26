3db040ac1390d48c40936c7140a43c3c12d5b423
sarahjanevincent <141277350+sarahjanevincent@users.noreply.github.com>
2025-03-26 10:53:14 -0400

    données à jour et loop graph

--- code/04_potGrowth/04_graph_rci_clusters.R
+++ code/04_potGrowth/04_graph_rci_clusters.R
@@ -30,25 +30,25 @@ cluster_info <- data.frame(
                  "_SharedFolder_datagotchi_federal_2024/images/Cluster_Datagotchi__0001_Jennifer2000.png", 
                  "_SharedFolder_datagotchi_federal_2024/images/Cluster_Datagotchi__0001_Jennifer2000.png"), 
   descriptionFr = c("Description de ce cluster:\n Jennifer est une Ontarienne\n urbaine de 50 ans\n faisant partie de la classe moyenne.\n Elle habite un condo,\n va au gym et prend\n son café au Starbucks.\n Elle se déplace en transport en commun.\n Elle aime le vin rouge\n et fréquente les musées.",
-                    "Électeurs conservateurs",
-                    "Pro-environnement",
-                    "Modérés",
-                    "Indécis",
-                    "Jeunes urbains",
-                    "Jeunes urbains",
-                    "Jeunes urbains",
-                    "Jeunes urbains",
-                    "Traditionnalistes"),
+                    "Description de ce cluster:\n Arjun est un immigrant Ontarien ou de l'Ouest de 45 ans. Il accompli souvent des tâches manuelles et aime les sports motorisés.",
+                    "Description de ce cluster:\n Michel est une Québécois francophone\n de 60 ans qui habite en région.\n Il est retraité.\n Il habite son duplex et\n aime faire des voyages à saveur historique.\n Il ne consulte pas beaucoup\n les réseaux sociaux. Il aime le vin\n et ne boit pas beaucoup de café.\n Il principalement une personne de matin.",
+                    "Description de ce cluster:\n David est un Ontarien éduqué et aisé\n de 50 ans qui habite dans une\n maison de ville. Il occupe un emploi professionnel.\n Il a un chien, va au gym,\n est prend son café au Tim Hortons. Il aime pêcher.",
+                    "Description de ce cluster:\n John est un albertain de 40 ans,\n qui occupe un emploi manuel et\n qui fait partie de la classe moyenne.\n Il fait des sports motorisés, chasse et pêche.\n Il mange de la viande à tous\n les repas ou presque. Il prend\n son café dans les chaines (surtout au Tim,\n mais McDonald's ça lui plait aussi).\n Il aime les boissons spiritueuses.",
+                    "Description de ce cluster:\n Maxime est un québécois de 35 ans.\n Il vit dans son appartement, fait du plein air\n et des sports motorisés.\n Il chasse et pêche à l'occasion.\n Il boit de la bière.",
+                    "Description de ce cluster:\n Zoe est une femme de 35 ans\n vivant dans un milieu urbain.\n Elle habite son appartement\n et occupe un emploi de professionnel ou de management.\n Elle se déplace à vélo et est tatouée.\n Elle consulte beaucoup instagram.\n Elle a un chat et un chien.\n Elle prend son café au Starbucks\n et fait de la course à pieds.",
+                    "Description de ce cluster:\n Julie est une québécoise de 50 ans\n peu éduquée avec peu de moyens.\n Elle habite son bloc appartement avec son chat\n et prend des marches.\n Elle ne boit pas de café et aime le vin.\n Elle est tatouée.",
+                    "Description de ce cluster:\n Robert est un homme des provinces atlantiques de 55 ans.\n Il habite sa maison et occupe un emploi manuel.\n Il pêche beaucoup, chasse à l'occasion.\n Il aime la bière et les boissons spiritueuses.\n Il prend son café au Tim Hortons.\n Il accompli souvent des tâches manuelles.",
+                    "Description de ce cluster:\n Emily est éduquée et vit dans un appartement ou un condo avec son chat. Elle est végétarienne. Elle fait du yoga et fréquente les cafés indépendants. Elle s'habille dans les friperies. Elle aime les cocktails et fume du pot à l'occasion."),
 descriptionEn = c("Cluster's description:\n Jennifer is a 50-year-old\n urban Ontarian who is part\n of the middle class.\n She lives in a condo,\n goes to the gym,\n gets her coffee at Starbucks.\n She uses public transit.\n She enjoys red wine\n and visits museums.",
-                  "Électeurs conservateurs",
-                  "Pro-environnement", 
-                  "Modérés",
-                  "Indécis",
-                  "Jeunes urbains",
-                  "Jeunes urbains",
-                  "Jeunes urbains",
-                  "Jeunes urbains",
-                  "Traditionnalistes"))
+                  "Cluster's description:\n Électeurs conservateurs",
+                  "Cluster's description:\n Pro-environnement", 
+                  "Cluster's description:\n Modérés",
+                  "Cluster's description:\n Indécis",
+                  "Cluster's description:\n Jeunes urbains",
+                  "Cluster's description:\n Jeunes urbains",
+                  "Cluster's description:\n Jeunes urbains",
+                  "Cluster's description:\n Jeunes urbains",
+                  "Cluster's description:\n Traditionnalistes"))
 
 # Charger l'image iceberg
 img_iceberg <- readPNG("_SharedFolder_datagotchi_federal_2024/images/icebergPixel.png")
@@ -375,3 +375,4 @@ ggsave(
   bg = "white",
   device = "png"
 )
+
